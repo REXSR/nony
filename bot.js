6af8745ded21039986d9937db2bef1c71b350615
REXSR <41483292+REXSR@users.noreply.github.com>
2018-09-11 02:27:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,6 +36,28 @@ client.on('message',async Epic => {
 
 
 
+client.on('message',async msg => {
+  if(msg.content.startsWith(p + "set2")) {
+  if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **go play minecraft**');
+  if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');
+  msg.guild.createChannel(`يتم تحضير الروم :[]` , 'voice').then(time => {
+    time.overwritePermissions(msg.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+  setInterval(() => {
+      var currentTime = new Date(),
+Year = currentTime.getFullYear(),
+Month = currentTime.getMonth() + 1,
+Dat = currentTime.getDate()
+      time.setName(`Members : ◤ → ${client.users.size} ← ◢`);
+ },1000);
+  });
+  }
+ 
+});
+ 
+   
 
 
 
